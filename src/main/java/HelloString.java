0a13d4f4315b82ac3de81dd34e3d63a20144fa4f
backcoding <liaoruohuai@126.com>
2017-09-01 07:18:43 +0800

    0.3

--- src/main/java/HelloString.java
+++ src/main/java/HelloString.java
@@ -2,4 +2,8 @@ public class HelloString {
     public String sayHello(){
         return "HelloWorld";
     }
+
+    public static void main(String[] args) {
+        System.out.println(new HelloString().sayHello());
+    }
 }
