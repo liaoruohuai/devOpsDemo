2c348665d66165f3ff1ca9c47b8f7ab77bd4a599
ase16_liaorh <liaoruohuai@126.com>
2017-09-04 13:56:06 +0800

    last one

--- src/main/java/HelloString.java
+++ src/main/java/HelloString.java
@@ -1,3 +1,5 @@
+import static java.lang.Thread.sleep;
+
 public class HelloString {
     public String sayHello(){
         return "HelloWorld";
@@ -5,5 +7,16 @@ public class HelloString {
 
     public static void main(String[] args) {
         System.out.println(new HelloString().sayHello());
+        long count=0;
+        while (1==1)
+        {
+            count++;
+            System.out.println(count + " Second Passed");
+            try{
+             sleep(1000);
+            }catch (InterruptedException e) {
+                System.err.println("Interrupted");
+            }
+        }
     }
 }
